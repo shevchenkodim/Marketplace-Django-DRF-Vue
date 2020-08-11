be96059e6b9d7e0984ad3bfcf383ce3b81913e9c
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-08-11 07:24:14 +0300

    Create base html file

--- clients/views.py
+++ clients/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     """Index page"""
-    template_name = "index.html"
+    template_name = "base.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
