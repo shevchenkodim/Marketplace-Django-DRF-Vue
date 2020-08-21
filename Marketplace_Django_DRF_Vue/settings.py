762f546ef8a094724ab9000cbad2ff9942a63fe3
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-08-21 05:49:17 +0300

    Create user manager

--- Marketplace_Django_DRF_Vue/settings.py
+++ Marketplace_Django_DRF_Vue/settings.py
@@ -23,8 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'clients',
     'common',
+    'clients',
 
     'mptt',
     'django_cleanup',
@@ -41,6 +41,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Marketplace_Django_DRF_Vue.urls'
+
+AUTH_USER_MODEL = 'common.auth.auth.User'
 
 TEMPLATES = [
     {
@@ -63,9 +65,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Marketplace_Django_DRF_Vue.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -73,9 +72,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,9 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'uk-ua'
 
 TIME_ZONE = 'Europe/Kiev'
@@ -106,9 +99,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
