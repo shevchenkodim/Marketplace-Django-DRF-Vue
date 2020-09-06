8b92692352bc180536b6465e0fa2fcf93a76a0f7
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-09-06 12:05:15 +0300

    Improve class Cart(add methods - len, get_toal_price, clear)

--- cart/cart.py
+++ cart/cart.py
@@ -26,3 +26,37 @@ class Cart(object):
 
     def save(self):
         self.session.modified = True
+
+    def remove(self, product):
+        """ Remove a product from the cart """
+        product_id = str(product.product_id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """ Iterate over the items in the cart and get the products from the database """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(product_id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.product_id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """ Count all items in the cart """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        """ Return total price in the cart """
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        """ Remove cart from session """
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
