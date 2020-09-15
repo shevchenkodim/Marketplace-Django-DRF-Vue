fcb83818c5dd68884b7d4b39b32849ba48b0e54e
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-09-15 06:54:19 +0300

    Connection celery to project

--- Marketplace_Django_DRF_Vue/settings.py
+++ Marketplace_Django_DRF_Vue/settings.py
@@ -112,3 +112,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CART_SESSION_ID = env('CART_SESSION_ID')
+
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKEN_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
