d0486835509fdbea246cd5123f83c1ab82fdc655
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-09-18 19:20:42 +0300

    Implement clear session auth data

--- client_api/auth/auth.py
+++ client_api/auth/auth.py
@@ -8,19 +8,24 @@ from broker.utils.otp_code.otp_code import GenerateOtpCode
 
 @api_view(['POST'])
 @client_or_none_only
-def client_sign_in(request):
-    """ Client SignIn """
+def client_auth(request):
+    """ Client Auth """
     if request.method == 'POST':
 
         return Response({}, status=status.HTTP_200_OK)
     return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-@client_or_none_only
-def client_sign_up(request):
-    """ Client SignUp """
-    if request.method == 'POST':
-
-        return Response(status=status.HTTP_200_OK)
-    return Response({}, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'POST'])
+def client_auth_init(request):
+    """ Client auth delete session data """
+    if request.session.get("auth_phone"):
+        del request.session["auth_phone"]
+    if request.session.get("auth_email"):
+        del request.session["auth_email"]
+    if request.session.get("auth_otp"):
+        del request.session["auth_otp"]
+    if request.session.get("auth_step"):
+        del request.session["auth_step"]
+    request.session.modified = True
+    return Response({}, status=status.HTTP_200_OK)
--- client_api/urls.py
+++ client_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from client_api.views.novelty.novelties import NoveltiesView
+from client_api.auth.auth import client_auth, client_auth_init
 
 app_name = 'client_api'
 urlpatterns = [
-    path('auth/', include('client_api.auth.urls')),
+    path('auth/', client_auth, name='auth'),
+    path('auth/init', client_auth_init, name='auth_init'),
 
     path('novelties', NoveltiesView.as_view(), name='novelties'),
 ]
