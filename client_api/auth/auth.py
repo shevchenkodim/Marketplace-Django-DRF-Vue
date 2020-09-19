fec61ecef3bcc3248ac35981c7b1cf8a789120de
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-09-19 11:30:28 +0300

    Implement auth api

--- client_api/auth/auth.py
+++ client_api/auth/auth.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import login
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from broker.utils.verification.verification import check_phone, clean_phone
+from broker.utils.verification.verification import check_phone, clean_phone, check_email
 from clients.utils.decorators import client_or_none_only, client_only
+from common.access.access import UserRole, AccessRole
 from common.models import User
 from broker.utils.otp_code.otp_code import GenerateOtpCode
 
@@ -21,20 +24,27 @@ def client_auth(request):
         if phone:
             phone = clean_phone(phone)
             if check_phone(phone):
+                auth_otp = get_otp_code()
+                request.session["auth_otp"] = auth_otp
+                request.session["auth_phone"] = phone
+                request.session["auth_step"] = second
+                request.session.modified = True
                 try:
                     client = User.objects.get(phone=phone)
-                    auth_otp = get_otp_code()
-                    request.session["auth_otp"] = auth_otp
-                    request.session["auth_phone"] = phone
-                    request.session["auth_step"] = second
-                    request.session.modified = True
                     state = {"phone": True, "phone_editable": False,
                              "otp": True, "otp_editable": False,
                              "password": True, "password_editable": True}
                     parameters = {"otp_code": auth_otp}
                     return Response({"state": state, "parameters": parameters}, status=status.HTTP_200_OK)
                 except User.DoesNotExist:
-                    pass
+                    state = {"phone": True, "phone_editable": False,
+                             "otp": True, "otp_editable": False,
+                             "password": True, "password_editable": True,
+                             "first_name": True, "first_name_editable": True,
+                             "last_name": True, "last_name_editable": True,
+                             "email": True, "email_editable": True}
+                    parameters = {"otp_code": auth_otp}
+                    return Response({"state": state, "parameters": parameters}, status=status.HTTP_200_OK)
             else:
                 errors = {"message": "Вкажіть вірний номер телефону"}
                 state = {"phone": True, "phone_editable": True}
@@ -44,7 +54,65 @@ def client_auth(request):
             state = {"phone": True, "phone_editable": True}
             return Response({"errors": errors, "state": state}, status=status.HTTP_200_OK)
     elif auth_step == second:
-        pass
+        phone = data.get("phone", None)
+        if phone:
+            phone = clean_phone(phone)
+            session_phone = request.session.get("auth_phone", None)
+            if not phone == session_phone:
+                errors = {"message": "Підміна телефону"}
+                state = {"show_phone": True, "phone_editable": False, "show_otp": False, "step": first}
+                return Response({"errors": errors, "state": state}, status=status.HTTP_200_OK)
+
+            otp_code = data.get("otp_code")
+            session_auth_otp = request.session.get("auth_otp", None)
+            if not str(otp_code) == str(session_auth_otp):
+                errors = {"message": "Невірній тимчасовий пароль"}
+                state = {"show_phone": True, "phone_editable": False, "otp_editable": False,
+                         "show_otp": True, "step": second}
+                return Response({"errors": errors, "state": state}, status=status.HTTP_200_OK)
+
+            try:
+                client = User.objects.get(phone=phone)
+                password = data.get("password")
+                if not client.check_password(password):
+                    errors = {"message": "Пароль невірний"}
+                    state = {"show_phone": True, "phone_editable": False,
+                             "show_otp": True, "otp_editable": False,
+                             "show_password": True, "step": second,
+                             "password_enabled": True}
+                    return Response({"errors": errors, "state": state}, status=status.HTTP_200_OK)
+                login(request, client)
+                return Response({"status": "ok", "redirect": reverse('client:index')}, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                first_name = data.get("first_name")
+                last_name = data.get("last_name")
+                password = data.get("password")
+                email = data.get("email")
+                if User.has_email(email):
+                    errors = {"message": "Такий email вже використовується"}
+                    return Response({"errors": errors}, status=status.HTTP_200_OK)
+                if not check_email(email):
+                    errors = {"message": "Email має невірний формат"}
+                    return Response({"errors": errors}, status=status.HTTP_200_OK)
+                if not first_name or not last_name:
+                    errors = {"message": "Потрібно ввести прізвище та ім'я"}
+                    return Response({"errors": errors}, status=status.HTTP_200_OK)
+                if not password or len(password) < 8:
+                    errors = {"message": "Пароль має бути не менше 8 символів"}
+                    return Response({"errors": errors}, status=status.HTTP_200_OK)
+                client = User.objects.create_user(
+                    email=email,
+                    phone=phone,
+                    first_name=first_name,
+                    last_name=last_name,
+                    password=password
+                )
+                UserRole.objects.create(
+                    role=AccessRole.objects.get_or_create(role='Клієнт', code_role='client')[0],
+                    user=client
+                )
+                login(request, client)
+                return Response({"status": "ok", "redirect": reverse('client:index')}, status=status.HTTP_200_OK)
     else:
         state = {"phone": True, "phone_editable": True}
     return Response({"state": state}, status=status.HTTP_200_OK)
--- common/models.py
+++ common/models.py
@@ -29,22 +29,16 @@ class User(AbstractBaseUser, PermissionsMixin):
         db_table = 'auth_user'
 
     def get_full_name(self):
-        '''
-        Returns the first_name plus the last_name, with a space in between.
-        '''
+        """ Returns the first_name plus the last_name, with a space in between. """
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
-        '''
-        Returns the short name for the user.
-        '''
+        """ Returns the short name for the user. """
         return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
-        Sends an email to this User.
-        '''
+        """ Sends an email to this User. """
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
     @staticmethod
@@ -54,3 +48,8 @@ class User(AbstractBaseUser, PermissionsMixin):
             return set(user_role.role.code_role for user_role in user_role_list)
         except Exception as e:
             return set()
+
+    @staticmethod
+    def has_email(email):
+        """ Does the user have such an email """
+        return User.objects.filter(email=email).exists()
