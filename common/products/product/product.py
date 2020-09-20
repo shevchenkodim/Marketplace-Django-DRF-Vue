679b8211442111220f20a5f8990afa0d7be809c8
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-09-20 12:04:34 +0300

    Add method for generate product_id code

--- common/products/product/product.py
+++ common/products/product/product.py
@@ -10,7 +10,7 @@ class Product(SeoModel):
     """ Product model """
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255, unique=True)
-    product_id = models.CharField(max_length=55, unique=True)
+    product_id = models.CharField(max_length=55, unique=True, db_index=True)
     is_available = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=15, decimal_places=2)
     old_price = models.DecimalField(max_digits=15, decimal_places=2)
@@ -29,9 +29,23 @@ class Product(SeoModel):
     # def get_absolute_url(self):
     #     return ''
 
-    def __str__(self):
-        return self.product_id
-
     class Meta:
         db_table = 'product'
         ordering = ['created_at']
+
+    def __str__(self):
+        return self.product_id
+
+    def save(self, *args, **kwargs):
+        if not self.product_id:
+            self.product_id = generate_product_id()
+        super(Product, self).save(*args, **kwargs)
+
+
+def generate_product_id():
+    """ Generate new product_id on create """
+    last_product = Product.objects.all().order_by('created_at').last()
+    last_value = last_product.product_id if last_product else '0000000000'
+    value_int = int(last_value) + 1
+    value_len = len(str(value_int))
+    return last_value[:-value_len] + str(value_int)
